dce5fd5f296b276f8333eb8f6da030eeece543b2
Mosesmpho <mosesmpho16@gmail.com>
2023-02-24 14:52:09 +0200

    Add project files.

---
+++ CapenexisLearners23/Data/CapenexisLearners23Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapenexisLearners23.Models;
+
+namespace CapenexisLearners23.Data
+{
+    public class CapenexisLearners23Context : DbContext
+    {
+        public CapenexisLearners23Context (DbContextOptions<CapenexisLearners23Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CapenexisLearners23.Models.Courses> Courses { get; set; } = default!;
+
+        public DbSet<CapenexisLearners23.Models.Facilitators> Facilitators { get; set; } = default!;
+
+        public DbSet<CapenexisLearners23.Models.Learners> Learners { get; set; } = default!;
+    }
+}
